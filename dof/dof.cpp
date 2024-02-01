50d6de1cd59236cc380a7e24529da5064a4b4f5c
starmaid <nicholas.masso.14@gmail.com>
2024-02-01 02:28:20 -0800

    adding second framebuffer so we can shader the first one

--- dof/dof.cpp
+++ dof/dof.cpp
@@ -18,25 +18,15 @@
 
 using namespace std;
 
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 enum class direction
 {
 	to_depth,
 	to_color
 };
-
-
-//const char* vertexShaderSource = "#version 330 core\n"
-//"layout (location = 0) in vec3 aPos;\n"
-//"void main()\n"
-//"{\n"
-//"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-//"}\0";
-//const char* fragmentShaderSource = "#version 330 core\n"
-//"out vec4 FragColor;\n"
-//"void main()\n"
-//"{\n"
-//"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-//"}\n\0";
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -50,6 +40,8 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 }
 
+
+
 int main(int argc, char* argv[])
 {
 	// init everything!!!
@@ -62,6 +54,12 @@ int main(int argc, char* argv[])
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
+
+
+
+
+	// ====================== start GL =============================
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 
@@ -90,49 +88,41 @@ int main(int argc, char* argv[])
 	if (GLEW_VERSION_3_3) { cout << "OpenGL 3.3 Supported" << endl; }
 
 
+
+
+
+
+	// =================== start realsense!!! ==================================
+	std::string serial;
+	if (!device_with_streams({ RS2_STREAM_COLOR,RS2_STREAM_DEPTH }, serial))
+		return EXIT_SUCCESS;
+
+	// Create a Pipeline - this serves as a top-level API for streaming and processing frames
+	rs2::pipeline pipe;
+	rs2::config cfg;
+	if (!serial.empty())
+		cfg.enable_device(serial);
+	cfg.enable_stream(RS2_STREAM_DEPTH);
+	cfg.enable_stream(RS2_STREAM_COLOR);
+	pipe.start(cfg);
+
+	// Define two align objects. One will be used to align
+	// to depth viewport and the other to color.
+	// Creating align object is an expensive operation
+	// that should not be performed in the main loop
+	rs2::align align_to_depth(RS2_STREAM_DEPTH);
+	rs2::align align_to_color(RS2_STREAM_COLOR);
+
+	float       alpha = 0.5f;               // Transparancy coefficient
+	direction   dir = direction::to_color;  // Alignment direction
+
+	texture depth_image, color_image;     // Helpers for renderig images
+
+
+
+
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-
-	// build and compile our shader program
-	// ------------------------------------
-	// vertex shader
-	//unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	//glCompileShader(vertexShader);
-	//// check for shader compile errors
-	//int success;
-	//char infoLog[512];
-	//glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	//if (!success)
-	//{
-	//	glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-	//	std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	//}
-	//// fragment shader
-	//unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	//glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	//glCompileShader(fragmentShader);
-	//// check for shader compile errors
-	//glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	//if (!success)
-	//{
-	//	glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-	//	std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	//}
-	//// link shaders
-	//unsigned int shaderProgram = glCreateProgram();
-	//glAttachShader(shaderProgram, vertexShader);
-	//glAttachShader(shaderProgram, fragmentShader);
-	//glLinkProgram(shaderProgram);
-	//// check for linking errors
-	//glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	//if (!success) {
-	//	glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-	//	std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	//}
-	//glDeleteShader(vertexShader);
-	//glDeleteShader(fragmentShader);
 
 	Shader ourShader("shaders/a.vert", "shaders/a.frag");
 
@@ -142,6 +132,30 @@ int main(int argc, char* argv[])
 		 0.5f, -0.5f, 0.0f, // right 
 		 0.0f,  0.5f, 0.0f  // top   
 	};
+
+	float quadVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+		// positions   // texCoords
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f,  0.0f, 0.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f, 1.0f
+	};
+
+	// screen quad VAO
+	unsigned int quadVAO, quadVBO;
+	glGenVertexArrays(1, &quadVAO);
+	glGenBuffers(1, &quadVBO);
+	glBindVertexArray(quadVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
 
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
@@ -162,6 +176,33 @@ int main(int argc, char* argv[])
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
 
+	
+
+
+	// framebuffer configuration
+	// -------------------------
+	unsigned int framebuffer;
+	glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+	// create a color attachment texture
+	unsigned int textureColorbuffer;
+	glGenTextures(1, &textureColorbuffer);
+	glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+	// create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+	unsigned int rbo;
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT); // use a single renderbuffer object for both a depth AND stencil buffer.
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+	// now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 
 
 	while (!glfwWindowShouldClose(window))
@@ -172,15 +213,82 @@ int main(int argc, char* argv[])
 
 		// render
 		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+		// first pass
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // we're not using the stencil buffer now
+		glEnable(GL_DEPTH_TEST);
+		//DrawScene();
+
+		// second pass
+		glBindFramebuffer(GL_FRAMEBUFFER, 0); // back to default
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		ourShader.use();
+		//glBindVertexArray(quadVAO);
+		glDisable(GL_DEPTH_TEST);
+		glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		ourShader.use();
+		glBindVertexArray(quadVAO);
+		glBindTexture(GL_TEXTURE_2D, textureColorbuffer);	// use the color attachment texture as the texture of the quad plane
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
+		// Using the align object, we block the application until a frameset is available
+		rs2::frameset frameset = pipe.wait_for_frames();
+
+		if (dir == direction::to_depth)
+		{
+			// Align all frames to depth viewport
+			frameset = align_to_depth.process(frameset);
+		}
+		else
+		{
+			// Align all frames to color viewport
+			frameset = align_to_color.process(frameset);
+		}
+
+		// With the aligned frameset we proceed as usual
+		auto depth = frameset.get_depth_frame();
+		auto color = frameset.get_color_frame();
+		//auto colorized_depth = c.colorize(depth);
+
+		glEnable(GL_BLEND);
+		// Use the Alpha channel for blending
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		if (dir == direction::to_depth)
+		{
+			// When aligning to depth, first render depth image
+			// and then overlay color on top with transparancy
+			//depth_image.render(colorized_depth, { 0, 0, app.width(), app.height() });
+			//color_image.render(color, { 0, 0, app.width(), app.height() }, alpha);
+		}
+		else
+		{
+			// When aligning to color, first render color image
+			// and then overlay depth image on top
+			//color_image.render(color, { 0, 0, 800, 600 });
+			//depth_image.render(colorized_depth, { 0, 0, app.width(), app.height() }, 1 - alpha);
+		}
+
+		//glColor4f(1.f, 1.f, 1.f, 1.f);
+		//glDisable(GL_BLEND);
+		glDeleteFramebuffers(1, &rbo);
+
+
+
 
 		// draw our first triangle
 		//glUseProgram(shaderProgram);
 		
-		ourShader.use();
-		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//ourShader.use();
+		//glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		// glBindVertexArray(0); // no need to unbind it every time 
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -194,62 +302,6 @@ int main(int argc, char* argv[])
 	//glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
-	return 0;
-
-
-	// start realsense!!!
-	std::string serial;
-	if (!device_with_streams({ RS2_STREAM_COLOR,RS2_STREAM_DEPTH }, serial))
-		return EXIT_SUCCESS;
-
-
-	// Create a Pipeline - this serves as a top-level API for streaming and processing frames
-	rs2::pipeline pipe;
-	rs2::config cfg;
-	if (!serial.empty())
-		cfg.enable_device(serial);
-	cfg.enable_stream(RS2_STREAM_DEPTH);
-	cfg.enable_stream(RS2_STREAM_COLOR);
-	pipe.start(cfg);
-
-	// Define two align objects. One will be used to align
-	// to depth viewport and the other to color.
-	// Creating align object is an expensive operation
-	// that should not be performed in the main loop
-	rs2::align align_to_depth(RS2_STREAM_DEPTH);
-	rs2::align align_to_color(RS2_STREAM_COLOR);
-
-	float       alpha = 0.5f;               // Transparancy coefficient
-	direction   dir = direction::to_depth;  // Alignment direction
-
-	
-
-	while (true) {
-        // Using the align object, we block the application until a frameset is available
-        rs2::frameset frameset = pipe.wait_for_frames();
-
-        if (dir == direction::to_depth)
-        {
-            // Align all frames to depth viewport
-            frameset = align_to_depth.process(frameset);
-        }
-        else
-        {
-            // Align all frames to color viewport
-            frameset = align_to_color.process(frameset);
-        }
-
-        // With the aligned frameset we proceed as usual
-        auto depth = frameset.get_depth_frame();
-        auto color = frameset.get_color_frame();
-
-		color.get_data_size();
-		color.get_data();
-
-
-
-
-	}
 
 
 	return 0;
